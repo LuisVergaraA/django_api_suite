9bd805263942a5cebc53b22e9c243a814c4d140e
LuisVergaraA <150173288+LuisVergaraA@users.noreply.github.com>
2025-07-27 18:58:19 +0000

    Guia 20 hasta GET

---
+++ demo_rest_api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+import uuid
+
+# Simulación de base de datos local en memoria
+data_list = []
+
+# Añadiendo algunos datos de ejemplo para probar el GET
+data_list.append({'id': str(uuid.uuid4()), 'name': 'User01', 'email': 'user01@example.com', 'is_active': True})
+data_list.append({'id': str(uuid.uuid4()), 'name': 'User02', 'email': 'user02@example.com', 'is_active': True})
+data_list.append({'id': str(uuid.uuid4()), 'name': 'User03', 'email': 'user03@example.com', 'is_active': False}) # Ejemplo de item inactivo
+
+class DemoRestApi(APIView):
+    name = "Demo REST API"
+
+    def get(self, request):
+        """
+        GET /demo/rest/api/index/
+        Retorna una lista de usuarios activos
+        """
+        # Filtra la lista para incluir solo los elementos donde 'is_active' es True
+        active_items = [item for item in data_list if item.get('is_active', False)]
+        
+        return Response({
+            'status': 'success',
+            'data': active_items,
+            'count': len(active_items)
+        }, status=status.HTTP_200_OK)
+
+
